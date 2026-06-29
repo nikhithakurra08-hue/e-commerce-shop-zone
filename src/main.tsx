f3de6b980937bd64408ef53f7df2c43e80191331
nikhithakurra08-hue <nikhithakurra08@gmail.com>
2026-06-29 13:02:17 +0530

    fix: add React Router v7 future flags to suppress upgrade warnings

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,10 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter future={{ 
+  v7_startTransition: true,
+  v7_relativeSplatPath: true 
+}}>
         <HelmetProvider>
           <App />
           <Toaster position="top-right" toastOptions={{ duration: 3000, style: { borderRadius: '8px', background: '#1a2535', color: '#fff' } }} />
